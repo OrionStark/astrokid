a363d105a6dc5caa9fb54f3943becba256f2cd5c
Robby Muhammad <robbybellamy6@gmail.com>
2017-12-23 05:16:08 +0700

    Folder trees changed

--- view/src/app/data-component/users/user.ts
+++
@@ -1,9 +0,0 @@
-export class User {
-    _id? : string;
-    name : string;
-    username : string;
-    password : string;
-    email : string;
-    data_saver : number;
-    constelation : string;
-}
